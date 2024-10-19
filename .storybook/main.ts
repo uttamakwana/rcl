f46ead0df65dacfc0a3ddafd578dc90f7010eae3
Uttam Makwana <uttamakwana4503@gmail.com>
2024-10-19 19:53:23 +0530

    feat: initialized storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,20 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/__docs__/*.stories.tsx", "../src/**/__docs__/*.mdx"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions"
+  ],
+  core: {
+    builder: "@storybook/builder-vite"
+  },
+  framework: {
+    name: "@storybook/react-vite",
+    options: {}
+  }
+};
+export default config;
---
+++ src/components/atomic/button/__docs__/example.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Button } from "../button.component";
+
+const Example = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%"
+      }}
+    >
+      <Button />
+    </div>
+  );
+};
+
+export default Example;
